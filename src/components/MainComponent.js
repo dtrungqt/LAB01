8cef2b7e9f59ef1aa1bacdabb0402e7ebec92e9c
DTrung <ngdinhtrung.qt@gmail.com>
2022-07-13 17:48:38 +0700

    react router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
+import Home from "./HomeComponent";
 import Menu from "./MenuComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import DishDetail from "./DishdetailComponent";
 import { DISHES } from "../shared/dishes";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 class Main extends Component {
   //định nghĩa state được import từ DISHES
@@ -12,37 +14,36 @@ class Main extends Component {
 
     this.state = {
       dishes: DISHES,
-      selectedDish: null,
-      //   trong Lab này, selectedDish chỉ theo dõi dishID chứ ko phải toàn bộ món ăn như ở Lab3 hay Ex1
     };
-  }
-  //onDishSelect là method để cập nhật selectedDish
-  onDishSelect(dishID) {
-    this.setState({ selectedDish: dishID });
   }
 
   render() {
+    const HomePage = () => {
+      return <Home />;
+    };
+
     return (
       <div>
         {/* Hiển thị Navbar  */}
         <Header />
-        {/* Hiển thị Menu món ăn  */}
-        <Menu
-          dishes={this.state.dishes}
-          onClick={(dishID) => this.onDishSelect(dishID)}
-        />
-        {/* Chuyển onClick dưới dạng property vào menu component, khi onClick được gọi sẽ chuyển tham số dishID cho onDishSelect */}
 
-        {/* Hiển thị DishDetail  */}
-        <DishDetail
-          dish={
-            this.state.dishes.filter(
-              (dish) => dish.id === this.state.selectedDish
-            )[0]
-          }
-        />
-        {/* Truyền vào DishDetail Component phần tử món ăn có id là dishID đã chọn từ Menu, onClick ở trên */}
-        {/* Kết quả của filter method là 1 hàm, ta chỉ chọn phần tử đầu tiên của hàm đó  */}
+        {/* Router  */}
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route
+            exact
+            path="/menu"
+            component={() => <Menu dishes={this.state.dishes} />}
+          />
+          {/* Sử dụng exact có nghĩa là path phải khớp chính xác với path sau đó (ở
+          đây là /menu). Bởi vì ta định tuyến (route) đến DishDetail Component
+          và sử dụng một đường dẫn bắt đầu bằng /menu */}
+
+          {/* Đặt đường dẫn mặc định */}
+          {/* nếu route không khớp với bất kỳ tuyến nào:
+          /home nay /menu thì sẽ chuyển hướng tới /home */}
+          <Redirect to="/home" />
+        </Switch>
 
         {/* Footer  */}
         <Footer />
